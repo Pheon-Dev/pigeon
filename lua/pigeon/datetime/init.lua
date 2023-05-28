c5dcb5f1e2c28d4cc6b1235a37211aed948f670e
Pheon-Dev <devpheon@gmail.com>
2023-05-29 00:17:18 +0300

    [~] returned result format

--- lua/pigeon/datetime/init.lua
+++ lua/pigeon/datetime/init.lua
@@ -3,15 +3,15 @@ local datetime = require("pigeon.config").options.datetime
 local M = {}
 
 M.current_day = function()
-	return datetime.day.icon .. " " .. os.date(datetime.day.format)
+	return datetime.day.icon .. os.date(datetime.day.format)
 end
 
 M.current_time = function()
-	return datetime.time.icon .. " " .. os.date(datetime.time.format) .. " " .. datetime.time.posttext
+	return datetime.time.icon .. os.date(datetime.time.format) .. datetime.time.posttext
 end
 
 M.current_date = function()
-	return datetime.date.icon .. " " .. os.date(datetime.date.format)
+	return datetime.date.icon .. os.date(datetime.date.format)
 end
 
 return M
