80d2abdc38e95880de332624f46918e01ead7f23
Pheon-Dev <devpheon@gmail.com>
2023-05-28 13:00:11 +0300

    [~] split wifi connection and signal speed

--- lua/pigeon/init.lua
+++ lua/pigeon/init.lua
@@ -66,24 +66,27 @@ M.battery = function()
 end
 
 M.wifi = function()
-	local connection = vim.fn.system('ping -c 1 8.8.8.8 > /dev/null 2>&1 && echo "  " || echo ""')
-	connection = connection:gsub("\n", "")
+	local result = vim.fn.system('ping -c 1 8.8.8.8 > /dev/null 2>&1 && echo "  " || echo ""')
+	result = result:gsub("\n", "")
 
-	local signal_speed = ""
+	return result
+end
+
+M.signal = function()
+	local result = ""
 
 	local function fetch_signal_speed()
 		local signal = vim.fn.systemlist('iwconfig 2>&1 | grep -o "Bit Rate=.*" | grep -o "[0-9.]*"')
 		if signal and #signal > 0 then
 			local speed = math.floor(tonumber(signal[1]) / 1)
-			signal_speed = speed .. " dBm"
+			result = speed .. " dBm"
 		else
-			signal_speed = "N/A"
+			result = "N/A"
 		end
 	end
 
 	fetch_signal_speed()
 
-	local result = connection .. " " .. signal_speed
 	return result
 end
 
