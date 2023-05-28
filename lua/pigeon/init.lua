78f48ef6f5ca2acbf27f7018a56232d363bb3096
Pheon-Dev <devpheon@gmail.com>
2023-05-28 17:45:54 +0300

    [+] all modules to the plugin

--- lua/pigeon/init.lua
+++ lua/pigeon/init.lua
@@ -4,6 +4,12 @@ local M = {}
 
 M.setup = config.setup
 
-M.battery = require("pigeon.battery").init()
+M.battery = require("pigeon.battery").capacity()
+M.battery = require("pigeon.battery").charge()
+M.wifi = require("pigeon.internet").wifi()
+M.signal = require("pigeon.internet").signal()
+M.date = require("pigeon.datetime").date()
+M.day = require("pigeon.datetime").day()
+M.time = require("pigeon.datetime").time()
 
 return M
