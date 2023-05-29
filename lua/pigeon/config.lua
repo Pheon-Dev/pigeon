0456c847afb60a0bd54ad52f5e5a0d4f660a011c
Pheon-Dev <devpheon@gmail.com>
2023-05-29 12:13:45 +0300

    [+] PigeonToggle command

--- lua/pigeon/config.lua
+++ lua/pigeon/config.lua
@@ -1,100 +1,101 @@
 local M = { options = {} }
 
 local defaults = {
-  os = "linux",            -- windows, osx
-  plugin_manager = "lazy", -- packer, paq, vim-plug
-  callbacks = {
-    killing_pigeon = nil,
-    respawning_pigeon = nil,
-  },
-  updates = {
-    pretext = "",
-    posttext = "",
-    icon = "󱌖",
-  },
-  datetime = {
-    time = {
-      format = "%H:%M",
-      posttext = "hrs",
-      icon = "",
-    },
-    day = {
-      format = "%A",
-      icon = "",
-    },
-    date = {
-      format = "%Y-%m-%d",
-      icon = "",
-    },
-  },
-  battery = {
-    show_percentage = true,
-    show_status_text = true,
-    view = {
-      charge = {
-        zeros = { icon = "" },
-        tens = { icon = "" },
-        twenties = { icon = "" },
-        thirties = { icon = "" },
-        forties = { icon = "" },
-        fifties = { icon = "" },
-        sixties = { icon = "" },
-        seventies = { icon = "" },
-        eighties = { icon = "" },
-        nineties = { icon = "" },
-        hundred = { icon = "" },
-      },
-      status = {
-        charging = { icon = " 󱐋" },
-        discharging = { icon = " 󱐌" },
-        not_charging = { icon = " " },
-        full = { icon = " ✔" },
-        unknown = { icon = " " },
-        percentage = { icon = " 󰏰" },
-      },
-    },
-  },
-  internet = {
-    signal = {
-      unit = "mbps", -- dBm, mbps | mb/s | Mb/s | MB/s | Mbps | MBps, Kbps, Gbps, Tbps
-    },
-    ethernet = {
-      icons = {
-        connected = "󰞉 ",
-        disconnected = "󰕑 ",
-      },
-    },
-    wifi = {
-      icons = {
-        connected = "󰤪",
-        disconnected = "󰤫",
-      },
-    },
-  },
-  volume = {
-    show_percentage = false,
-    icon = "󱄠",
-  },
-  temperature = {
-    show_percentage = false,
-    icon = "",
-  },
-  storage = {
-    show_percentage = false,
-    icon = "󱛟",
-  },
-  ram = {
-    show_percentage = false,
-    icon = "󰍛",
-  },
-  cpu = {
-    show_percentage = false,
-    icon = "󰻠",
-  },
+	enabled = true,
+	os = "linux", -- windows, osx
+	plugin_manager = "lazy", -- packer, paq, vim-plug
+	callbacks = {
+		killing_pigeon = nil,
+		respawning_pigeon = nil,
+	},
+	updates = {
+		pretext = "",
+		posttext = "",
+		icon = "󱌖",
+	},
+	datetime = {
+		time = {
+			format = "%H:%M",
+			posttext = "hrs",
+			icon = "",
+		},
+		day = {
+			format = "%A",
+			icon = "",
+		},
+		date = {
+			format = "%Y-%m-%d",
+			icon = "",
+		},
+	},
+	battery = {
+		show_percentage = true,
+		show_status_text = true,
+		view = {
+			charge = {
+				zeros = { icon = "" },
+				tens = { icon = "" },
+				twenties = { icon = "" },
+				thirties = { icon = "" },
+				forties = { icon = "" },
+				fifties = { icon = "" },
+				sixties = { icon = "" },
+				seventies = { icon = "" },
+				eighties = { icon = "" },
+				nineties = { icon = "" },
+				hundred = { icon = "" },
+			},
+			status = {
+				charging = { icon = " 󱐋" },
+				discharging = { icon = " 󱐌" },
+				not_charging = { icon = " " },
+				full = { icon = " ✔" },
+				unknown = { icon = " " },
+				percentage = { icon = " 󰏰" },
+			},
+		},
+	},
+	internet = {
+		signal = {
+			unit = "mbps", -- dBm, mbps | mb/s | Mb/s | MB/s | Mbps | MBps, Kbps, Gbps, Tbps
+		},
+		ethernet = {
+			icons = {
+				connected = "󰞉 ",
+				disconnected = "󰕑 ",
+			},
+		},
+		wifi = {
+			icons = {
+				connected = "󰤪",
+				disconnected = "󰤫",
+			},
+		},
+	},
+	volume = {
+		show_percentage = false,
+		icon = "󱄠",
+	},
+	temperature = {
+		show_percentage = false,
+		icon = "",
+	},
+	storage = {
+		show_percentage = false,
+		icon = "󱛟",
+	},
+	ram = {
+		show_percentage = false,
+		icon = "󰍛",
+	},
+	cpu = {
+		show_percentage = false,
+		icon = "󰻠",
+	},
 }
 
 M.load = function(opts)
-  M.options = vim.tbl_deep_extend("force", defaults, opts or {})
+	M.options = vim.tbl_deep_extend("force", defaults, opts or {})
 end
 
 return M
---
+++ plugin/pigeon.lua
@@ -0,0 +1,16 @@
+if vim.g.loaded_pigeon then
+	return
+end
+
+vim.g.loaded_pigeon = true
+
+local command = vim.apu.nvim_create_user_command
+local config = require("pigeon.config")
+
+command("PigeonToggle", function()
+	require("pigeon").toggle()
+end, {})
+
+if config.options.enabled then
+	require("pigeon").respawn()
+end
