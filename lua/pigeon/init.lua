766e17e80cca07532a601a23e401b235c537471c
Pheon-Dev <devpheon@gmail.com>
2023-05-30 17:44:28 +0300

    [+] toggle each individual submodules

--- lua/pigeon/init.lua
+++ lua/pigeon/init.lua
@@ -1,31 +1,6 @@
 local config = require("pigeon.config")
-local callback = require("pigeon.utils.data").do_callback
-local pigeon_feeding
 
 local M = {}
-
-function M.respawn()
-	callback("respawning_pigeon")
-	pigeon_feeding = true
-	require("pigeon.config").options.enabled = pigeon_feeding
-end
-
-function M.kill()
-	vim.api.nvim_create_augroup("KillPigeon", { clear = true })
-	callback("killing_pigeon")
-	pigeon_feeding = false
-	require("pigeon.config").options.enabled = pigeon_feeding
-end
-
-function M.toggle()
-	if pigeon_feeding then
-		M.kill()
-		print("󱗆 pigeon killed")
-	else
-		M.respawn()
-		print("󱗆 pigeon respawned")
-	end
-end
 
 M.setup = function(opts)
 	vim.validate({ opts = { opts, "table", true } })
--- plugin/pigeon.lua
+++ plugin/pigeon.lua
@@ -3,19 +3,3 @@ if vim.g.loaded_pigeon then
 end
 
 vim.g.loaded_pigeon = true
-
-local command = vim.api.nvim_create_user_command
-local config = require("pigeon.config")
-
-command("PigeonToggleBattery", function()
-	config.options.battery.enabled = not config.options.battery.enabled
-  print(config.options.battery.enabled)
-end, {})
-
-command("PigeonToggle", function()
-	require("pigeon").toggle()
-end, {})
-
-if config.options.enabled then
-	require("pigeon").respawn()
-end
