5732589785c6d46a51c984a1f14ed5ff5339d00c
Pheon-Dev <devpheon@gmail.com>
2023-05-31 12:23:53 +0300

    [+] datetime commands and notifications

--- lua/pigeon/datetime/init.lua
+++ lua/pigeon/datetime/init.lua
@@ -1,4 +1,7 @@
 local datetime = require("pigeon.config").options.datetime
+local date = require("pigeon.config").options.datetime.date
+local time = require("pigeon.config").options.datetime.time
+local day = require("pigeon.config").options.datetime.day
 local command = vim.api.nvim_create_user_command
 
 local M = {}
@@ -14,6 +17,36 @@ end
 M.current_date = function()
 	return datetime.date.icon .. os.date(datetime.date.format)
 end
+
+command("PigeonToggleTime", function()
+	time.enabled = not time.enabled
+	if time.enabled then
+		print("󱗆 pigeon time submodule is enabled")
+	end
+	if not time.enabled then
+		print("󱗆 pigeon time submodule is disabled")
+	end
+end, {})
+
+command("PigeonToggleDay", function()
+	day.enabled = not day.enabled
+	if day.enabled then
+		print("󱗆 pigeon day submodule is enabled")
+	end
+	if not day.enabled then
+		print("󱗆 pigeon day submodule is disabled")
+	end
+end, {})
+
+command("PigeonToggleDate", function()
+	date.enabled = not date.enabled
+	if date.enabled then
+		print("󱗆 pigeon date submodule is enabled")
+	end
+	if not date.enabled then
+		print("󱗆 pigeon date submodule is disabled")
+	end
+end, {})
 
 command("PigeonToggleDateTime", function()
 	datetime.enabled = not datetime.enabled
