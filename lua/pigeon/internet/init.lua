653d3fa5ec53024a9d9ff50300e9736069a290e8
Pheon-Dev <devpheon@gmail.com>
2023-06-01 15:29:55 +0300

    [+] using job_start to get the essid

--- lua/pigeon/internet/init.lua
+++ lua/pigeon/internet/init.lua
@@ -2,80 +2,91 @@ local internet = require("pigeon.config").options.internet
 
 local M = {}
 
+local cmd = 'iwconfig 2>&1 | grep "ESSID:" | awk -F "ESSID:" \'{print $2}\' | tr -d \'"\''
+
 M.wifi_status = function()
-	-- local wifi_state = vim.fn.systemlist('iwconfig 2>&1 | grep -o "ESSID:.*" | grep -o "[Aa0-Zz9.]*"')
-	-- wifi_state = tostring(wifi_state[2])
-	local cmd = 'iwconfig 2>&1 | grep "ESSID:" | awk -F "ESSID:" \'{print $2}\' | tr -d \'"\''
+  local job_id = vim.fn.jobstart(cmd, {
+    on_stdout = function(_, data, _)
+      local output = table.concat(data, "\n")
 
-	local job_id = vim.fn.jobstart(cmd, {
-		on_stdout = function(_, data, _)
-			local output = table.concat(data, "\n")
+      -- local wifi_status
+      if output and #output > 0 then
+        output = output:gsub("%s+", "") -- Remove whitespace
+        if output == "off/any" then
+          vim.g.wifi_status = "disconnected"
+        else
+          vim.g.wifi_status = "connected"
+        end
+        print("󱗆 pigeon wifi status: " .. vim.g.wifi_status)
+      else
+        vim.g.wifi_status = "unknown"
+      end
 
-			-- local wifi_status
-			if output and #output > 0 then
-				output = output:gsub("%s+", "") -- Remove whitespace
-				if output == "off/any" then
-					vim.g.wifi_status = "disconnected"
-				else
-					vim.g.wifi_status = "connected"
-				end
-			else
-				vim.g.wifi_status = "unknown"
-			end
+      -- TODO: Do something with the wifi_status, such as updating the statusline
+    end,
+    stdout_buffered = true,
+  })
 
-			-- TODO: Do something with the wifi_status, such as updating the statusline
-			-- print("󱗆 pigeon wifi_status: " .. vim.g.wifi_status)
-		end,
-		stdout_buffered = true,
-	})
+  vim.fn.jobwait({ job_id }, 0)
+  local result
 
-	vim.fn.jobwait({ job_id }, 0)
-	local result
+  if vim.g.wifi_status == "disconnected" then
+    result = internet.wifi.icons.disconnected
+  elseif vim.g.wifi_status == "connected" then
+    result = internet.wifi.icons.connected
+  elseif vim.g.wifi_status == "unknown" then
+    result = ""
+  end
 
-	if vim.g.wifi_status == "disconnected" then
-		result = internet.wifi.icons.disconnected
-	else
-		result = internet.wifi.icons.connected
-	end
-
-	return result
-	-- -- Set up a timer to check if the job has finished
-	-- local timer = vim.loop.new_timer()
-	-- timer:start(
-	-- 	100,
-	-- 	0,
-	-- 	vim.schedule_wrap(function()
-	-- 		local status = vim.fn.jobwait({ job_id }, 0)
-	-- 		if status > 0 then
-	-- 			-- Job has finished, stop the timer
-	-- 			timer:stop()
-	-- 		end
-	-- 	end)
-	-- )
+  return result
 end
 
 M.wifi_essid = function()
-	local wifi_state = vim.fn.systemlist('iwconfig 2>&1 | grep -o "ESSID:.*" | grep -o "[Aa0-Zz9.]*"')
-	wifi_state = tostring(wifi_state[2])
-	return wifi_state
+  local job_id = vim.fn.jobstart(cmd, {
+    on_stdout = function(_, data, _)
+      local output = table.concat(data, "\n")
+
+      -- local wifi_status
+      if output and #output > 0 then
+        output = output:gsub("%s+", "") -- Remove whitespace
+        if output == "off/any" then
+          vim.g.wifi_essid = "offline"
+        else
+          vim.g.wifi_essid = output
+        end
+        print("󱗆 pigeon connected to: " .. vim.g.wifi_status)
+      else
+        vim.g.wifi_status = "unknown"
+      end
+
+      -- TODO: Do something with the wifi_status, such as updating the statusline
+    end,
+    stdout_buffered = true,
+  })
+
+  vim.fn.jobwait({ job_id }, 0)
+
+  -- local wifi_state = vim.fn.systemlist('iwconfig 2>&1 | grep -o "ESSID:.*" | grep -o "[Aa0-Zz9.]*"')
+  -- wifi_state = tostring(wifi_state[2])
+  return vim.g.wifi_essid
 end
 
 M.bit_rate = function()
-	local result = ""
-	local unit = internet.signal.unit
+  local result = ""
+  local unit = internet.signal.unit
 
-	local function fetch_signal_speed()
-		local signal = vim.fn.systemlist('iwconfig 2>&1 | grep -o "Bit Rate=.*" | grep -o "[0-9.]*"')
-		if signal and #signal > 0 then
-			result = tonumber(signal[1]) .. " " .. unit
-		else
-			result = " 󰪎"
-		end
-	end
+  local function fetch_signal_speed()
+    local signal = vim.fn.systemlist('iwconfig 2>&1 | grep -o "Bit Rate=.*" | grep -o "[0-9.]*"')
+    if signal and #signal > 0 then
+      result = tonumber(signal[1]) .. " " .. unit
+    else
+      result = " 󰪎"
+    end
+  end
 
-	fetch_signal_speed()
+  fetch_signal_speed()
 
-	return result
+  return result
 end
 
 require("pigeon.commands.internet").internet_commands()
