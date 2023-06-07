5054bfc077179c117103f4f7ab735f2393bacd51
Pheon-Dev <devpheon@gmail.com>
2023-06-07 14:43:18 +0300

    [+] ram commands

---
+++ lua/pigeon/commands/ram.lua
@@ -0,0 +1,31 @@
+local ram = require("pigeon.config").options.ram
+local command = vim.api.nvim_create_user_command
+local M = {}
+
+function M.ram_commands()
+	command("PigeonToggleRam", function()
+		ram.enabled = not ram.enabled
+		if ram.enabled then
+			print("󱗆 pigeon ram module is enabled")
+		end
+		if not ram.enabled then
+			print("󱗆 pigeon ram module is disabled")
+		end
+	end, {})
+
+	command("PigeonToggleRamPercentage", function()
+		ram.show_percentage = not ram.show_percentage
+		if ram.show_percentage then
+			print("󱗆 pigeon ram percentage is enabled")
+		end
+		if not ram.show_percentage then
+			print("󱗆 pigeon ram percentage is disabled")
+		end
+	end, {})
+
+	if not ram.enabled then
+		print("󱗆 pigeon ram module is disabled")
+	end
+end
+
+return M
