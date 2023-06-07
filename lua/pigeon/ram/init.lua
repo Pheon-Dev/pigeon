4c0c2b92e43660f78fc012a74260ca624a219f06
Pheon-Dev <devpheon@gmail.com>
2023-06-07 13:16:49 +0300

    [+] ram module

---
+++ lua/pigeon/ram/init.lua
@@ -0,0 +1,55 @@
+local ram = require("pigeon.config").options.ram
+
+local M = {}
+
+M.total_ram = function()
+  local cmd = 'free -mh --si | awk  {"print $2"} | head -n 2 | tail -1'
+  local job_id = vim.fn.jobstart(cmd, {
+    on_stdout = function(_, data, _)
+      local output = table.concat(data, "\n")
+
+      if output and #output > 0 then
+        output = output:gsub("%s+", "") -- Remove whitespace
+        vim.g.total_ram = output
+      else
+        vim.g.total_ram = ""
+      end
+    end,
+    stdout_buffered = true,
+  })
+
+  vim.fn.jobwait({ job_id }, 0)
+  local result
+
+  result = vim.g.total_ram
+
+  return result
+end
+
+M.used_ram = function()
+  local cmd = 'free -mh --si | awk  {"print $3"} | head -n 2 | tail -1'
+  local job_id = vim.fn.jobstart(cmd, {
+    on_stdout = function(_, data, _)
+      local output = table.concat(data, "\n")
+
+      if output and #output > 0 then
+        output = output:gsub("%s+", "") -- Remove whitespace
+        vim.g.used_ram = output
+      else
+        vim.g.used_ram = "0"
+      end
+    end,
+    stdout_buffered = true,
+  })
+
+  vim.fn.jobwait({ job_id }, 0)
+  local result
+
+  result = vim.g.used_ram
+
+  return result
+end
+
+M.ram_icon = ram.icon
+
+return M
