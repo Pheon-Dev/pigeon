e5cb03e81b554c60905035cc5b6423fecf03dba3
Pheon-Dev <devpheon@gmail.com>
2023-06-07 13:35:21 +0300

    [+] ram used percentage

--- lua/pigeon/ram/init.lua
+++ lua/pigeon/ram/init.lua
@@ -3,51 +3,59 @@ local ram = require("pigeon.config").options.ram
 local M = {}
 
 M.total_ram = function()
-	local cmd = "free -mh --si | awk  {'print $2'} | head -n 2 | tail -1"
-	local job_id = vim.fn.jobstart(cmd, {
-		on_stdout = function(_, data, _)
-			local output = table.concat(data, "\n")
+  local cmd = "free -mh --si | awk  {'print $2'} | head -n 2 | tail -1"
+  local job_id = vim.fn.jobstart(cmd, {
+    on_stdout = function(_, data, _)
+      local output = table.concat(data, "\n")
 
-			if output and #output > 0 then
-				output = output:gsub("%s+", "") -- Remove whitespace
-				vim.g.total_ram = output
-			else
-				vim.g.total_ram = ""
-			end
-		end,
-		stdout_buffered = true,
-	})
+      if output and #output > 0 then
+        output = output:gsub("%s+", "") -- Remove whitespace
+        vim.g.total_ram = output
+      else
+        vim.g.total_ram = ""
+      end
+    end,
+    stdout_buffered = true,
+  })
 
-	vim.fn.jobwait({ job_id }, 0)
-	local result
+  vim.fn.jobwait({ job_id }, 0)
+  local result
 
-	result = vim.g.total_ram
+  result = vim.g.total_ram
 
-	return result
+  return result
+end
+
+M.perc_ram = function()
+  local result
+
+  result = vim.g.used_ram * 100 / vim.g.total_ram
+
+  return result
 end
 
 M.used_ram = function()
-	local cmd = "free -mh --si | awk  {'print $3'} | head -n 2 | tail -1"
-	local job_id = vim.fn.jobstart(cmd, {
-		on_stdout = function(_, data, _)
-			local output = table.concat(data, "\n")
+  local cmd = "free -mh --si | awk  {'print $3'} | head -n 2 | tail -1"
+  local job_id = vim.fn.jobstart(cmd, {
+    on_stdout = function(_, data, _)
+      local output = table.concat(data, "\n")
 
-			if output and #output > 0 then
-				output = output:gsub("%s+", "") -- Remove whitespace
-				vim.g.used_ram = output
-			else
-				vim.g.used_ram = "0"
-			end
-		end,
-		stdout_buffered = true,
-	})
+      if output and #output > 0 then
+        output = output:gsub("%s+", "") -- Remove whitespace
+        vim.g.used_ram = output
+      else
+        vim.g.used_ram = "0"
+      end
+    end,
+    stdout_buffered = true,
+  })
 
-	vim.fn.jobwait({ job_id }, 0)
-	local result
+  vim.fn.jobwait({ job_id }, 0)
+  local result
 
-	result = vim.g.used_ram
+  result = vim.g.used_ram
 
-	return result
+  return result
 end
 
 M.ram_icon = ram.icon
