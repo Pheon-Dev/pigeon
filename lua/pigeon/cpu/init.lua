650766cd946bdd557d0bde2728361b61cae0b9ec
Pheon-Dev <devpheon@gmail.com>
2023-06-08 11:33:10 +0300

    [+] calculate cpu usage

--- lua/pigeon/cpu/init.lua
+++ lua/pigeon/cpu/init.lua
@@ -4,55 +4,60 @@ local percentage = require("pigeon.config").options.percentage
 local M = {}
 
 M.cpu_load = function()
-	local PREFIX = ""
+  local cmd_last = "head -n1 /proc/stat"
+  local cmd_now = "sleep 0.05 && head -n1 /proc/stat | awk '{print $2}'"
+  local last_job_id = vim.fn.jobstart(cmd_last, {
+    on_stdout = function(_, data, _)
+      local output = table.concat(data, "\n")
 
-	-- Get the first line with aggregate of all CPUs
-	local stat_file = io.popen("cat /proc/stat")
-	local cpu_last = {}
-	for line in stat_file:lines() do
-		cpu_last = line:split("%s+")
-		break
-	end
-	stat_file:close()
+      if output and #output > 0 then
+        output = output:gsub("%s+", "") -- Remove whitespace
+        local cpu_sum = 0
+        for i = 2, #output do
+          cpu_sum = cpu_sum + tonumber(output[i])
+        end
+        vim.g.last_cpu_read = cpu_sum
+        vim.g.last_cpu_idle = tonumber(output[5])
+      else
+        vim.g.last_cpu_read = 0
+      end
+    end,
+    stdout_buffered = true,
+  })
 
-	local cpu_last_sum = 0
-	for i = 2, #cpu_last do
-		cpu_last_sum = cpu_last_sum + tonumber(cpu_last[i])
-	end
+  vim.fn.jobwait({ last_job_id }, 0)
 
-	os.execute("sleep 0.05")
+  local now_job_id = vim.fn.jobstart(cmd_now, {
+    on_stdout = function(_, data, _)
+      local output = table.concat(data, "\n")
 
-	stat_file = io.popen("cat /proc/stat")
-	local cpu_now = {}
-	for line in stat_file:lines() do
-		cpu_now = line:split("%s+")
-		break
-	end
-	stat_file:close()
+      if output and #output > 0 then
+        output = output:gsub("%s+", "") -- Remove whitespace
+        local cpu_sum = 0
+        for i = 2, #output do
+          cpu_sum = cpu_sum + tonumber(output[i])
+        end
+        vim.g.now_cpu_read = cpu_sum
+        vim.g.now_cpu_idle = tonumber(output[5])
+      else
+        vim.g.now_cpu_read = 0
+      end
+    end,
+    stdout_buffered = true,
+  })
 
-	local cpu_sum = 0
-	for i = 2, #cpu_now do
-		cpu_sum = cpu_sum + tonumber(cpu_now[i])
-	end
+  vim.fn.jobwait({ now_job_id }, 0)
 
-	local cpu_delta = cpu_sum - cpu_last_sum
-	local cpu_idle = tonumber(cpu_now[5]) - tonumber(cpu_last[5])
-	local cpu_used = cpu_delta - cpu_idle
-	local cpu_usage = 100 * cpu_used / cpu_delta
+  vim.g.cpu_delta = vim.g.now_cpu_read - vim.g.last_cpu_read
+  vim.g.cpu_idle = vim.g.now_cpu_idle - vim.g.last_cpu_idle
+  vim.g.cpu_used = vim.g.now_cpu_delta - vim.g.cpu_idle
 
-	-- Keep this as last for our next read
-	cpu_last = cpu_now
-	cpu_last_sum = cpu_sum
+  local result = 100 * vim.g.cpu_used / vim.g.cpu_delta
 
-	-- if cpu_usage == 100 then
-	--   print(PREFIX .. " " .. cpu_usage .. "%")
-	-- elseif cpu_usage >= 10 then
-	--   print(PREFIX .. "  " .. cpu_usage .. "%")
-	-- elseif cpu_usage <= 9 then
-	--   print(PREFIX .. "   " .. cpu_usage .. "%")
-	-- end
-	return cpu_usage
+  return result
 end
+
+M.cpu_icon = cpu.icon
 
 -- require("pigeon.commands.cpu").cpu_commands()
 
