87be659e4a5e153d28570293d8a08a629919758f
Pheon-Dev <devpheon@gmail.com>
2023-09-03 13:11:56 +0300

    [+] mute state and pulsemixer
    Co-authored-by: NStefan002

--- lua/pigeon/volume/init.lua
+++ lua/pigeon/volume/init.lua
@@ -3,54 +3,73 @@ local volume = require("pigeon.config").options.volume
 local M = {}
 
 function M.volume_job()
-	local cmd_right = "amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }'"
-	local cmd_left = "amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }'"
+  local cmd_right, cmd_left, mute
+  if vim.fn.executable("amixer") == 1 then
+    cmd_right = "amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }'"
+    cmd_left = "amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }'"
+  elseif vim.fn.executable("pulsemixer") == 1 then
+    cmd_right = "pulsemixer --get-volume | awk -F ' ' '{ print $1 }'"
+    cmd_left = "pulsemixer --get-volume | awk -F ' ' '{ print $2 }'"
+    mute = "pulsemixer --get-mute"
+  end
 
-	local right_job_id = vim.fn.jobstart(cmd_right, {
-		on_stdout = function(_, data, _)
-			local output = data[1]
+  local right_job_id = vim.fn.jobstart(cmd_right, {
+    on_stdout = function(_, data, _)
+      local output = data[1]
 
-			if output and #output > 0 then
-				output = string.gsub(output, "%%", "")
-				vim.g.volume_right = output
-			end
-		end,
-		stdout_buffered = true,
-	})
+      if output and #output > 0 then
+        output = string.gsub(output, "%%", "")
+        vim.g.volume_right = output
+      end
+    end,
+    stdout_buffered = true,
+  })
 
-	local left_job_id = vim.fn.jobstart(cmd_left, {
-		on_stdout = function(_, data, _)
-			local output = data[1]
+  local left_job_id = vim.fn.jobstart(cmd_left, {
+    on_stdout = function(_, data, _)
+      local output = data[1]
 
-			if output and #output > 0 then
-				output = string.gsub(output, "%%", "")
-				vim.g.volume_left = output
-			end
-		end,
-		stdout_buffered = true,
-	})
+      if output and #output > 0 then
+        output = string.gsub(output, "%%", "")
+        vim.g.volume_left = output
+      end
+    end,
+    stdout_buffered = true,
+  })
 
-	vim.fn.jobwait({ right_job_id }, 0)
-	vim.fn.jobwait({ left_job_id }, 0)
+  local mute_job_id = vim.fn.jobstart(mute, {
+    on_stdout = function(_, data, _)
+      local output = data[1]
+
+      if output and #output > 0 then
+        output = string.gsub(output, "%%", "")
+        vim.g.mute = output
+      end
+    end,
+    stdout_buffered = true,
+  })
+  vim.fn.jobwait({ right_job_id }, 0)
+  vim.fn.jobwait({ left_job_id }, 0)
+  vim.fn.jobwait({ mute_job_id }, 0)
 end
 
 function M.volume()
-	M.volume_job()
-	local result = volume.icon .. " "
-	if vim.g.volume_left then
-		result = result .. vim.g.volume_left .. " 󰏰"
-	else
-		result = result .. "..."
-	end
+  M.volume_job()
+  local result = volume.icon .. " "
+  if vim.g.volume_left then
+    result = result .. vim.g.volume_left .. " 󰏰"
+  else
+    result = result .. "..."
+  end
 
-	result = result .. "  "
-	if vim.g.volume_right then
-		result = result .. vim.g.volume_right .. " 󰏰"
-	else
-		result = result .. "..."
-	end
+  result = result .. "  "
+  if vim.g.volume_right then
+    result = result .. vim.g.volume_right .. " 󰏰"
+  else
+    result = result .. "..."
+  end
 
-	return result
+  return vim.g.mute and "Muted" or result
 end
 
 require("pigeon.commands.volume").volume_commands()
